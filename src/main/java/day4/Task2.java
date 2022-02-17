d2bdbbb1fde03d26a856987da43b538c81f4f5f2
LuroyJenkins <starkfinity@gmail.com>
2022-02-17 23:16:52 +0500

    optimizing day1-4 solutions

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -6,23 +6,25 @@ public class Task2 {
     public static void main(String[] args) {
         int[] mas = new int[100];
         Random rand = new Random();
-        int max = 0, min = 0, zeros = 0, sum_zeros = 0;
+        int count_zeros = 0, sum_zeros = 0;
 
         for (int i=0;i<mas.length;i++) {
             mas[i] = rand.nextInt(10000);
         }
+        int max = mas[0];
+        int min = mas[0];
 
         for (int Mas : mas) {
             if (Mas > max) max = Mas;
             if (Mas < min) min = Mas;
             if (Mas % 10 == 0) {
-                zeros++;
+                count_zeros++;
                 sum_zeros += Mas;
             }
         }
         System.out.println("Наибольший элемент массива: " + max);
         System.out.println("Наименьший элемент массива: " + min);
-        System.out.println("Количество элементов массива, оканчивающихся на 0: " + zeros);
+        System.out.println("Количество элементов массива, оканчивающихся на 0: " + count_zeros);
         System.out.println("Сумма элементов массива, оканчивающихся на 0: " + sum_zeros);
     }
 }
--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -6,17 +6,21 @@ public class Task4 {
     public static void main(String[] args) {
         int[] mas = new int[100];
         Random rand = new Random();
-        int sum_three_max = 0, first_num_three = 0;
+        int sum_three_max = 0, first_num_three = 0, current_sum;
 
         for (int i = 0; i < mas.length; i++) {  // заполняем массив
             mas[i] = rand.nextInt(10000);
         }
 
-        for(int i=1; i<mas.length-1;i++){
-            int current_sum = mas[i-1]+mas[i]+mas[i+1];
-            if(current_sum>sum_three_max){
+        for (int i = 1; i < mas.length - 1; i++) {
+            current_sum = 0;
+            //int current_sum = mas[i-1]+mas[i]+mas[i+1];
+            for (int j = 0; j < 3; j++) {
+                current_sum += mas[i-1+j];
+            }
+            if (current_sum > sum_three_max) {
                 sum_three_max = current_sum;
-                first_num_three = i-1;
+                first_num_three = i - 1;
             }
         }
 
