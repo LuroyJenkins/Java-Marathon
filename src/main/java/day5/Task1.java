26fbce6f937c4977b61d41666f3d31714293cd37
LuroyJenkins <starkfinity@gmail.com>
2022-02-17 22:44:54 +0500

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,44 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car Tesla = new Car();
+        Tesla.setColor("white");
+        Tesla.setModel("X");
+        Tesla.setYear(2022);
 
+        System.out.println("Цвет автомобиля: "+Tesla.getColor());
+        System.out.println("Модель автомобиля: "+Tesla.getModel());
+        System.out.println("Год выпуска: "+Tesla.getYear());
+
+    }
+}
+
+class Car {
+    private String Model;
+    private String Color;
+    private int year;
+
+    public void setModel(String Model) {
+        this.Model = Model;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,64 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorcycle m0 = new Motorcycle();
+        Motorcycle m1 = new Motorcycle("Suzuki");
+        Motorcycle m2 = new Motorcycle("Harley", "Red");
+        Motorcycle m3 = new Motorcycle("Honda", "Black", 2021 );
+    }
+}
 
+class Motorcycle{
+    private String Model;
+    private String Color;
+    private int year;
+
+    public Motorcycle(){               // конструктор по умолчанию, вызывается когда никакие параметры не передаются
+        this.Model = "default";
+        this.Color = "default";
+        this.year = 0;
+        System.out.println("Default constructor!");
+    }
+    public Motorcycle(String Model){     // 1 параметр
+        this.Model = Model;
+        this.Color = "default";
+        this.year = 0;
+        System.out.println("1 param constructor!");
+    }
+    public Motorcycle(String Model, String Color){     // 2 параметра
+        this.Model = Model;
+        this.Color = Color;
+        this.year = 0;
+        System.out.println("2 params constructor!");
+    }
+    public Motorcycle(String Model, String Color, int year){  // 3 параметра
+        this.Model = Model;
+        this.Color = Color;
+        this.year = year;
+        System.out.println("3 params constructor!");
+    }
+
+    public void setModel(String Model) {
+        this.Model = Model;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
