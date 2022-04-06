84d34f4cc97f4f6f54c1b288bf06a6df4ce80a4d
LuroyJenkins <starkfinity@gmail.com>
2022-04-06 18:36:34 +0500

    complete day14 tasks

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,40 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("src/main/java/day14/Test");
+        printSumDigits(file);
 
     }
+
+    public static void printSumDigits (File file){
+        Scanner scanner = null;
+        int sum = 0;
+
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
+        String File = scanner.nextLine();
+        String[] numbers = File.split(" ");
+
+        try {
+            if (numbers.length != 10)
+                throw new IllegalArgumentException();
+            for (String number: numbers) {
+                sum += Integer.parseInt(number);
+            }
+            System.out.println(sum);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Некорректный входной файл");
+        }
+    }
 }
+
--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,40 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file = new File("src/main/java/day14/people");
+        System.out.println(parseFileToStringList(file));
+    }
 
+    public static List<String> parseFileToStringList(File file){
+        Scanner scanner;
+        List<String> nameList = new ArrayList<>();
+
+        try {
+            scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                try{
+                    String line = scanner.nextLine();
+                    String[] nameLine = line.split(" ");
+                    if(Integer.parseInt(nameLine[1]) < 0)
+                        throw new IllegalArgumentException();
+                    nameList.add(line);
+                }
+                catch(IllegalArgumentException e){
+                    System.out.println("Некорректный входной файл");
+                    nameList.clear();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
+        return nameList;
     }
 }
