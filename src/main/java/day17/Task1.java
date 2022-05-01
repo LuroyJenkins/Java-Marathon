97573562ff2c136bfa9626cce0dabc1f7d53036c
LuroyJenkins <starkfinity@gmail.com>
2022-05-01 18:15:25 +0500

    complete day17 tasks

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,10 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        ChessPiece[] chessPiece = {ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+                                   ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK};
+        for(ChessPiece piece: chessPiece){
+            System.out.print(piece);
+        }
     }
 }
